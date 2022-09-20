27557fc728b5de6dda7bd20c4d7a77ad670f7c58
Brent Forwood <forwoodb77@gmail.com>
2022-09-20 01:45:28 -0400

    First commit

---
+++ blog/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+
+STATUS = (
+    (0, "Draft"),
+    (1, "Publish")
+)
+
+FRONT = (
+    (0, "None"),
+    (1, "Top"),
+    (2, "Secondary")
+)
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='blog_posts')
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    front = models.IntegerField(choices=FRONT, default=0)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.title
